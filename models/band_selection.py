ec7feb6ef29e0fff81e96290a0df1bc1308d4fd1
KikkiZ <zhangyozh@foxmail.com>
2023-12-10 21:11:26 +0800

    docs: 更新文档注释

--- models/band_selection.py
+++ models/band_selection.py
@@ -1,7 +1,15 @@
 import torch
 
 
-def band_recombination(data: torch.Tensor):
+def structure_info(data: torch.Tensor):
+    """ 计算一组图像间的结构相似度
+    默认输入的图像为(C,W,H), 通过拉普拉斯矩阵计算
+    该组图像的结构相似度, 并返回一张二维矩阵,行号和
+    列号分别代表比较的双方, 对应的值为二者的结构相似度
+
+    :param data: 输入需要分组的图像
+    :return: 返回一张二维矩阵
+    """
     if len(data.shape) == 3:
         band = data.shape[0]
         laplace = laplace_matrix(data)
@@ -15,6 +23,13 @@ _sigma_r = 0.5
 
 
 def adjacent_matrix(data: torch.Tensor) -> torch.Tensor:
+    """ 计算输入图像的邻接矩阵
+    可以输入单张(2D)图像或多张图像(3D),
+    函数会自动计算, 并返回相同通道数的邻接矩阵
+
+    :param data: 输入需要计算的图像
+    :return: 返回图像对应的邻接矩阵
+    """
     def _delta(size: int):
         _row = torch.arange(size).unsqueeze(1).expand(size, size).cuda()  # 行
         _col = torch.arange(size).unsqueeze(0).expand(size, size).cuda()  # 列
@@ -53,6 +68,13 @@ def adjacent_matrix(data: torch.Tensor) -> torch.Tensor:
 
 
 def diag_matrix(data: torch.Tensor) -> torch.Tensor:
+    """ 计算图像的度矩阵
+    输入一张或一组图像, 默认输入的图像为(C,W,H),
+    函数将自动生成一张度矩阵, 并转换为对角矩阵
+
+    :param data: 输入需要计算的图像
+    :return: 返回图像对应的度矩阵
+    """
     kernel = torch.ones(1, 1, 3, 3).cuda()
     kernel[0][0][1][1] = 0
 
@@ -70,7 +92,14 @@ def diag_matrix(data: torch.Tensor) -> torch.Tensor:
     return torch.diag(diag)
 
 
-def laplace_matrix(data: torch.Tensor):
+def laplace_matrix(data: torch.Tensor) -> torch.Tensor:
+    """ 计算图像的拉普拉斯矩阵
+    输入一张或一组图像, 默认输入的图像为(W,H)/(C,W,H),
+    函数将分别计算邻接矩阵和度矩阵, 并计算出拉普拉斯矩阵
+
+    :param data: 输入需要计算的图像
+    :return: 返回图像对应的拉普拉斯矩阵
+    """
     if len(data.shape) == 3:
         band = data.shape[0]
 
