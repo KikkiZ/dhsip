baa7e8de2fa8500332bc848a148b154d8f9c374d
KikkiZ <zhangyozh@foxmail.com>
2023-12-12 00:59:38 +0800

    feat: 完善大批量结构相似度快速计算函数

--- models/band_selection.py
+++ models/band_selection.py
@@ -1,4 +1,49 @@
+import math
+
 import torch
+
+
+def band_recombination(data: torch.Tensor):
+    """ 将一张高光谱图像按结构相似度进行分组
+    输入一张高光谱图像, 图像结构为(C,W,H),
+    通过计算出的结构相似度, 将光谱图像按相似度重组
+
+    :param data: 需要重组的高光谱图像
+    :return: 返回重组完的图像
+    """
+    if len(data.shape) != 3:
+        raise ValueError('The input tensor dimension is incorrect')
+
+    data = data.cuda()
+    sub_image_size = 16
+    band, width, height = data.shape
+    if width < sub_image_size * 2 and height < sub_image_size * 2:
+        # 当输入数据大小未超过阈值时, 可以直接进行结构相似度计算
+        similarity = structure_info(data)
+
+    else:
+        num_row = math.ceil(width / sub_image_size)
+        num_col = math.ceil(height / sub_image_size)
+
+        # 将输入数据切割为指定大小
+        blocks = []
+        row_blocks = torch.chunk(data, num_row, dim=1)
+        for row_block in row_blocks:
+            col_blocks = torch.chunk(row_block, num_col, dim=2)
+            blocks.extend(col_blocks)
+
+        # 计算结构相似度
+        similarity = []
+        for block in blocks:
+            info = structure_info(block)
+            similarity.append(info)
+
+        similarity = torch.stack(similarity, dim=0)  # 将分组的相似度合并, 通道数为分组数量
+        similarity = torch.sum(similarity, dim=0)    # 将相似度数据叠加, 减少通道数
+
+    # TODO 波段重组
+
+    return similarity
 
 
 def structure_info(data: torch.Tensor) -> torch.Tensor:
@@ -12,16 +57,39 @@ def structure_info(data: torch.Tensor) -> torch.Tensor:
     """
     if len(data.shape) == 3:
         band = data.shape[0]
+        data = data.cuda()
         laplace = laplace_matrix(data)
 
-        structural_similarity = torch.zeros(band, band)
-        for i in range(band):
-            for j in range(i + 1, band):
-                # @ 为矩阵乘
-                i = laplace[i] @ laplace[j] - laplace[j] @ laplace[i]
-                structural_similarity[i][j] = torch.norm(i).item() ** 2
+        # 由于直接计算内存占用过高, 需要按批次计算
+        batch_size = 3000                                   # 每一批的大小
+        num_batch = math.ceil(band ** 2 / batch_size)       # 分批计算的总批次
+        indices = torch.triu_indices(band, band, offset=1)  # 生成上三角矩阵的索引, 共两个列表, 分别存有x和y轴
+        indices_x = indices[0]                              # 获取需要进行计算的第一个张量的索引列表
+        indices_y = indices[1]                              # 获取第二个张量的索引列表
 
-        return structural_similarity
+        structural_similarity = []
+        for batch in range(num_batch):
+            temp_x = indices_x[batch * batch_size: (batch + 1) * batch_size]
+            temp_y = indices_y[batch * batch_size: (batch + 1) * batch_size]
+            laplace_x = laplace[temp_x]
+            laplace_y = laplace[temp_y]
+
+            # 'ijk,ikl->ijl' 使用了爱因斯坦求和约定来描述操作运算
+            # 'ijk,ikl' 表示输入两个输入张量维度的标签, 'ijl'表示输出张量的维度的标签
+            # 当标签输入维度标签相同时视为求和, 不同的维度标签被视为进行乘法
+            # 该式描述的操作: i维度上进行求和; jk和kl进行矩阵乘, 生成jl大小的矩阵; 最终生成了ijl大小的矩阵
+            info = (torch.einsum('ijk,ikl->ijl', laplace_x, laplace_y) -
+                    torch.einsum('ijk,ikl->ijl', laplace_y, laplace_x))
+            info = torch.norm(info, dim=[1, 2]).pow(2)
+            structural_similarity.extend(info.tolist())
+
+        structural_similarity = torch.tensor(structural_similarity)
+
+        upper_triangular = torch.zeros(band, band)
+        indices = torch.triu_indices(band, band, offset=1)
+        upper_triangular[indices[0], indices[1]] = structural_similarity
+
+        return upper_triangular
 
     else:
         raise ValueError('The input tensor dimension is incorrect')
@@ -39,6 +107,7 @@ def adjacent_matrix(data: torch.Tensor) -> torch.Tensor:
     :param data: 输入需要计算的图像
     :return: 返回图像对应的邻接矩阵
     """
+
     def _delta(size: int):
         _row = torch.arange(size).unsqueeze(1).expand(size, size).cuda()  # 行
         _col = torch.arange(size).unsqueeze(0).expand(size, size).cuda()  # 列
@@ -113,7 +182,7 @@ def laplace_matrix(data: torch.Tensor) -> torch.Tensor:
         band = data.shape[0]
 
         diag = diag_matrix(data)
-        diag = diag[None, :]              # 拓展对角矩阵的维度
+        diag = diag[None, :]  # 拓展对角矩阵的维度
         diag = diag.expand(band, -1, -1)  # 将对角矩阵拓展到输入数据相同通道数
 
         adjacent = adjacent_matrix(data)
