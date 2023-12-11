ff68893a5bf92ac42747de8cd43752d79ed390b0
KikkiZ <zhangyozh@foxmail.com>
2023-12-12 00:59:38 +0800

    docs: 完善文档和注释

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -2,6 +2,11 @@ import scipy.io as sio
 
 
 def read_file(file_dir: str):
+    """ 从指定位置读取 .mat文件
+
+    :param file_dir: 需要读取的文件的位置
+    :return: 读取出的数据
+    """
     mat = sio.loadmat(file_dir)
 
     data = dict()
