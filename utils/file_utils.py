98f41fd9f8b17aec886515f4a6512239b8e5f6cf
KikkiZ <zhangyozh@foxmail.com>
2023-12-14 23:32:55 +0800

    refactor: 重构相关文件工具

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,26 +1,11 @@
-from typing import Dict, TypeVar
+from typing import Dict, TypeVar, Union
 
+import numpy
 import scipy.io as sio
 import torch
 
 
 T = TypeVar('T')
-
-
-def read_file(file_dir: str) -> Dict[str, T]:
-    """ 从指定位置读取 .mat文件
-
-    :param file_dir: 需要读取的文件的位置
-    :return: 读取出的数据
-    """
-    mat = sio.loadmat(file_dir)
-
-    data = dict()
-    for key in mat.keys():
-        if not key.startswith('__') and not key.endswith('__'):
-            data[key] = mat[key]
-
-    return data
 
 
 def read_data(file_dir: str) -> Dict[str, torch.Tensor]:
@@ -39,10 +24,14 @@ def read_data(file_dir: str) -> Dict[str, torch.Tensor]:
     return data
 
 
-def save_file(file_dir: str, data: Dict[str, T]):
+def save_data(file_dir: str, data: Dict[str, torch.Tensor]):
     """ 存储数据到指定位置
 
     :param file_dir: 存储文件的位置
     :param data: 需要存储的数据
     """
+    for key in data.keys():
+        if data[key].is_cuda:
+            data[key] = data[key].cpu().detach().numpy()
+
     sio.savemat(file_dir, data)
