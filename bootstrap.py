1d9608e696409566e3546d0f4b69a543fb23247b
KikkiZ <zhangyozh@foxmail.com>
2023-12-18 16:52:48 +0800

    refactor: 重构代码结构

--- bootstrap.py
+++ bootstrap.py
@@ -1,26 +1,28 @@
 import argparse
+import datetime
 
-import denoising_2d
-import denoising_band
-import denoising_red
-from models.band_selection import band_recombination
+import torch
+from torch.utils.tensorboard import SummaryWriter
+
+import deep_hsi_prior
+from models.band_selection import band_recombination, band_recovery
 from utils.data_utils import print_image
-from utils.file_utils import read_data
+from utils.file_utils import read_data, save_data
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='deep_hs_prior')
 
-    parser.add_argument('--net', dest='net', default='default', type=str)  # 网络选择
-    parser.add_argument('--num_iter', dest='num_iter', default=3000, type=int)  # 网络迭代次数
-    parser.add_argument('--optimizer', dest='optimizer', default='adam', type=str)  # 优化器
+    # TODO 删除不必要的参数
+    parser.add_argument('--net', dest='net', default='default', type=str)
+    parser.add_argument('--num_iter', dest='num_iter', default=3000, type=int)
     parser.add_argument('--reg_noise_std', dest='reg_noise_std', default=0.03, type=float)
     parser.add_argument('--show_every', dest='show_every', default=50, type=int)
     parser.add_argument('--exp_weight', dest='exp_weight', default=0.99, type=float)
     parser.add_argument('--lr', dest='learning_rate', default=0.01, type=float)
     parser.add_argument('--skip_channel', dest='skip_channel', default=4, type=int)
-    parser.add_argument('--up_channel', dest='up_channel', default=128, type=int)
-    parser.add_argument('--down_channel', dest='down_channel', default=128, type=int)
+    parser.add_argument('--up_channel', dest='up_channel', nargs='+', default=[128, 128, 128, 128, 128], type=int)
+    parser.add_argument('--down_channel', dest='down_channel', nargs='+', default=[128, 128, 128, 128, 128], type=int)
     parser.add_argument('--upsample_mode', dest='upsample_mode', default='bilinear', type=str)
     parser.add_argument('--downsample_mode', dest='downsample_mode', default='stride', type=str)
 
@@ -37,18 +39,48 @@ if __name__ == '__main__':
     image = data_dict['image'].cuda()
     decrease_image = data_dict['image_noisy'].cuda()
     print_image([image, decrease_image], title='origin image')
-    print(decrease_image.shape)
 
-    if args.net == '2d':
-        denoising_2d.func(args, image, decrease_image)
+    if args.net == 'base':
+        date = datetime.datetime.now().strftime('%Y-%m-%d.%H-%M-%S')
+        writer = SummaryWriter('./logs/denoising/' + date)
+
+        output, _ = deep_hsi_prior.func(args, image, decrease_image, args.net, writer=writer)
+        writer.close()
 
     elif args.net == 'red':
-        denoising_red.func(args, image, decrease_image)
+        date = datetime.datetime.now().strftime('%Y-%m-%d.%H-%M-%S')
+        writer = SummaryWriter('./logs/denoising_red/' + date)
+
+        output, _ = deep_hsi_prior.func(args, image, decrease_image, args.net, writer=writer)
+        writer.close()
 
     elif args.net == 'band':
         group, recombination_image = band_recombination(decrease_image, group_size=5)  # 将图像按结构相似度分组
+        print(group)
 
-        denoising_band.func(args, image, recombination_image, group)
+        date = datetime.datetime.now().strftime('%Y-%m-%d.%H-%M-%S')
+        log_dir = './logs/denoising_band/' + date + '/'
+
+        output = []
+        output_avg = []
+        for index, (image_indices, decrease_subimage) in enumerate(zip(group, recombination_image)):
+            print('epoch: [' + str(index + 1) + '/' + str(len(group)) + ']')
+
+            date = datetime.datetime.now().strftime('%Y-%m-%d.%H-%M-%S')
+            writer = SummaryWriter(log_dir + date)
+
+            origin_subimage = torch.index_select(image, dim=0, index=torch.tensor(image_indices).cuda())
+            out, out_avg = deep_hsi_prior.func(args, origin_subimage, decrease_subimage, mode='red', writer=writer)
+            output.append(out)
+            output_avg.append(out_avg)
+
+            writer.close()
+
+        output = band_recovery(group, output).cuda()
+        output_avg = band_recovery(group, output_avg).cuda()
+        print_image([output.detach()])
+        save_data('./data/output.pth',
+                  {'output': output.detach_().clone(), 'output_avg': output_avg.detach_().clone()})
 
     else:
-        raise ValueError('The input parameter is incorrect, you need to choose between 2d, red and band')
+        raise ValueError('The input parameter is incorrect, you need to choose between base, red and band')
--- deep_hsi_prior.py
+++ deep_hsi_prior.py
@@ -1,13 +1,13 @@
-import datetime
 import time
+from typing import Tuple
 
-import numpy as np
+import numpy
 import torch
 from torch.utils.tensorboard import SummaryWriter
 
 from models.non_local_means import non_local_means
 from models.unet2D import UNet
-from utils.data_utils import print_image, min_max_normalize, psnr
+from utils.data_utils import min_max_normalize, print_image, psnr
 from utils.model_utils import noise_generator
 
 torch.backends.cudnn.enabled = True
@@ -15,7 +15,11 @@ torch.backends.cudnn.benchmark = True
 data_type = torch.cuda.FloatTensor
 
 
-def func(args, image, decrease_image):
+def func(args,
+         image: torch.Tensor,
+         decrease_image: torch.Tensor,
+         mode: str = 'base',
+         writer: SummaryWriter = None) -> Tuple[torch.Tensor, torch.Tensor]:
 
     reg_noise_std = args.reg_noise_std  # 扰动噪声张量的常量
     learning_rate = args.learning_rate  # 学习率
@@ -27,8 +31,8 @@ def func(args, image, decrease_image):
 
     net = UNet(image.shape[0],
                image.shape[0],
-               num_channels_up=[args.up_channel] * 5,
-               num_channels_down=[args.down_channel] * 5,
+               num_channels_up=args.up_channel,
+               num_channels_down=args.down_channel,
                num_channel_skip=args.skip_channel,
                kernel_size_up=3,
                kernel_size_down=3,
@@ -43,26 +47,22 @@ def func(args, image, decrease_image):
     net.to(device)
     print('module running in: ', device)
 
-    s = sum([np.prod(list(p.size())) for p in net.parameters()])  # 计算模型参数
+    s = sum([numpy.prod(list(p.size())) for p in net.parameters()])  # 计算模型参数
     print('number of params: ', s)
-    print('memory occupied by parameter: ', s * 4 / 1024 / 1024, 'MB')
 
     # 该值代表损失函数约束的拉格朗日乘子张量
     lagrange_multiplier = torch.zeros(image.shape).type(data_type)[None, :].cuda()
     # 该值初始以退化图像作为基准值, 后续会不断更新该值
     benchmark_image = decrease_image.clone().type(data_type)[None, :].cuda()
     temp_benchmark = benchmark_image.clone()  # 基准值的副本, 用于中间计算
+    out = None
     out_avg = None                            # 用于记录平滑输出的累计值
 
     # 拓展数据的维度
     decrease_image = decrease_image[None, :].cuda()
     # 自动生成一个指定维度的噪声图像
     net_input = noise_generator('2D', list(image.shape)).type(data_type).detach()
-    net_input_saved = net_input.clone().detach_()  # clone the noise tensor without grad
-    noise = net_input.clone().detach_()            # clone twice
-
-    date = datetime.datetime.now().strftime('%Y-%m-%d.%H-%M-%S')
-    writer = SummaryWriter('./logs/denoising_red/' + date)  # the location where the data record is saved
+    noise = net_input.clone().detach_()
 
     print('start iteration...')
     start_time = time.time()
@@ -76,55 +76,60 @@ def func(args, image, decrease_image):
     for i in range(num_iter + 1):
         optimizer.zero_grad()  # 清空梯度
 
-        inputs = net_input_saved + (noise.normal_() * reg_noise_std)  # 扰动每一轮的网络输入噪声
+        inputs = net_input + (noise.normal_() * reg_noise_std)  # 扰动每一轮的网络输入噪声
         out = net(inputs)
 
-        temp = benchmark_image - lagrange_multiplier
-        loss_net = criterion(out, decrease_image)  # 网络的loss
-        loss_red = criterion(out, temp.detach_())  # RED's loss
+        if mode == 'base':
+            total_loss = criterion(out, decrease_image)
+        elif mode == 'red':
+            temp = benchmark_image - lagrange_multiplier
+            loss_net = criterion(out, decrease_image)  # 网络的loss
+            loss_red = criterion(out, temp.detach_())  # RED's loss
 
-        total_loss = loss_net + mu * loss_red
+            total_loss = loss_net + mu * loss_red
+
+            benchmark_image = 1 / (beta + mu) * (beta * temp_benchmark + mu * (out + lagrange_multiplier))
+            lagrange_multiplier = lagrange_multiplier + out - benchmark_image
+        else:
+            raise TypeError('incorrect mode value')
 
         total_loss.backward()
         optimizer.step()  # update the parameters of the model
 
-        # 模型每迭代一定次数更新一次基准值
-        if i % 25 == 0:
+        # 当模式为red时, 模型每迭代一定次数更新一次基准值
+        if i % 25 == 0 and mode == 'red':
             temp_benchmark = non_local_means(benchmark_image.clone().squeeze(), 3)
 
-            msg = 'benchmark: [' + str(i) + '/' + str(num_iter) + ']'
-            benchmark_image_normalize = min_max_normalize(benchmark_image.squeeze().detach())
-            lagrange_multiplier_normalize = min_max_normalize(lagrange_multiplier.squeeze().detach())
-            print_image([benchmark_image_normalize, lagrange_multiplier_normalize], title=msg)
-
-        benchmark_image = 1 / (beta + mu) * (beta * temp_benchmark + mu * (out + lagrange_multiplier))
-        lagrange_multiplier = lagrange_multiplier + out - benchmark_image
+            # msg = 'benchmark: [' + str(i) + '/' + str(num_iter) + ']'
+            # benchmark_image_normalize = min_max_normalize(benchmark_image.squeeze().detach())
+            # lagrange_multiplier_normalize = min_max_normalize(lagrange_multiplier.squeeze().detach())
+            # print_image([benchmark_image_normalize, lagrange_multiplier_normalize], title=msg)
 
         if out_avg is None:
             out_avg = out.detach()
         else:
             out_avg = out_avg * exp_weight + out.detach() * (1 - exp_weight)
 
-        psnr_noisy = psnr(decrease_image.squeeze(), out.squeeze())
-        psnr_gt = psnr(image, out.squeeze())
-        psnr_gt_sm = psnr(image, out_avg.squeeze())
-        psnr_temp = psnr(image, benchmark_image.squeeze())
-        psnr_temp_u = psnr(image, (benchmark_image - lagrange_multiplier).squeeze())
+        # 当需要记录数据时才计算psnr
+        if writer is not None:
+            psnr_noisy = psnr(decrease_image.squeeze(), out.squeeze())
+            psnr_gt = psnr(image, out.squeeze())
+            psnr_gt_sm = psnr(image, out_avg.squeeze())
 
-        writer.add_scalar('compare with de', psnr_noisy, i)
-        writer.add_scalar('compare with gt', psnr_gt, i)
-        writer.add_scalar('compare with gt_sm', psnr_gt_sm, i)
-        writer.add_scalar('compare with temp', psnr_temp, i)
-        writer.add_scalar('compare with temp_u', psnr_temp_u, i)
+            writer.add_scalar('compare with de', psnr_noisy, i)
+            writer.add_scalar('compare with gt', psnr_gt, i)
+            writer.add_scalar('compare with gt_sm', psnr_gt_sm, i)
 
         if i % show_every == 0:
             msg = 'iteration times: [' + str(i) + '/' + str(num_iter) + ']'
             print(msg)
 
-            out_normalize = min_max_normalize(out.squeeze().detach())
-            out_avg_normalize = min_max_normalize(out_avg.squeeze().detach())
-            print_image([out_normalize, out_avg_normalize], title=msg)
+            if out.shape[0] > 100:
+                out_normalize = min_max_normalize(out.squeeze().detach())
+                out_avg_normalize = min_max_normalize(out_avg.squeeze().detach())
+                print_image([out_normalize, out_avg_normalize], title=msg)
 
-    writer.close()
     end_time = time.time()
     print('cost time', end_time - start_time, 's')
+
+    return out.squeeze(), out_avg.squeeze()
